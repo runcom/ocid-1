a89c6bc1e49efa71eb9fd8d67ffcf6671aa6eaae
Antonio Murdaca <runcom@redhat.com>
2016-12-11 17:45:40 +0100

    wip
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- server/image_status.go
+++ server/image_status.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"github.com/Sirupsen/logrus"
+	"github.com/containers/storage/storage"
 	"golang.org/x/net/context"
 	pb "k8s.io/kubernetes/pkg/kubelet/api/v1alpha1/runtime"
 )
@@ -16,6 +17,9 @@ func (s *Server) ImageStatus(ctx context.Context, req *pb.ImageStatusRequest) (*
 	}
 	status, err := s.images.ImageStatus(ctx, image)
 	if err != nil {
+		if err == storage.ErrImageUnknown {
+			return &pb.ImageStatusResponse{}, nil
+		}
 		return nil, err
 	}
 	return &pb.ImageStatusResponse{
