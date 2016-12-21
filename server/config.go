intelsdi-x/cri-o

Integrate containers/storage
Use containers/storage to store images, pod sandboxes, and containers.
A pod sandbox's infrastructure container has the same ID as the pod to
which it belongs, and all containers also keep track of their pod's ID.

The container configuration that we build using the data in a
CreateContainerRequest is stored in the container's ContainerDirectory,
and we use that file as a template.  Each time the container is mounted,
we read the template, replace its rootfs location with the mountpoint
for the container's layer, and save the result to the container's
ContainerRunDirectory, which we use as the bundle directory.

We catch SIGTERM and SIGINT, and when we receive either, we gracefully
exit the grpc loop.  If we also think that there aren't any container
filesystems in use, we attempt to do a clean shutdown of the storage
driver.

The test harness now waits for ocid to exit before attempting to delete
the storage root directory.

Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

server/config.go
@@ -22,17 +22,20 @@ type Config struct {
 
 // RootConfig represents the root of the "ocid" TOML config table.
 type RootConfig struct {
-	// Root is a path to the "root directory" where all information not
+	// Root is a path to the "root directory" where data not
 	// explicitly handled by other options will be stored.
 	Root string `toml:"root"`
 
-	// SandboxDir is the directory where ocid will store all of its sandbox
-	// state and other information.
-	SandboxDir string `toml:"sandbox_dir"`
+	// RunRoot is a path to the "run directory" where state information not
+	// explicitly handled by other options will be stored.
+	RunRoot string `toml:"runroot"`
 
-	// ContainerDir is the directory where ocid will store all of its container
-	// state and other information.
-	ContainerDir string `toml:"container_dir"`
+	// Storage is the name of the storage driver which handles actually
+	// storing the contents of containers.
+	Storage string `toml:"storage_driver"`
+
+	// StorageOption is a list of storage driver specific options.
+	StorageOption []string `toml:"storage_option"`
 
 	// LogDir is the default log directory were all logs will go unless kubelet
 	// tells us to put them somewhere else.
@@ -68,17 +71,15 @@ type RuntimeConfig struct {
 
 // ImageConfig represents the "ocid.image" TOML config table.
 type ImageConfig struct {
-	// Pause is the path to the statically linked pause container binary, used
-	// as the entrypoint for infra containers.
-	//
-	// TODO(cyphar): This should be replaced with a path to an OCI image
-	// bundle, once the OCI image/storage code has been implemented.
-	Pause string `toml:"pause"`
-
-	// ImageStore is the directory where the ocid image store will be stored.
-	// TODO: This is currently not really used because we don't have
-	//       containers/storage integrated.
-	ImageDir string `toml:"image_dir"`
+	// DefaultTransport is a value we prefix to image names that fail to
+	// validate source references.
+	DefaultTransport string `toml:"default_transport"`
+	// PauseImage is the name of an image which we use to instantiate infra
+	// containers.
+	PauseImage string `toml:"pause_image"`
+	// PauseCommand is the path of the binary we run in an infra
+	// container that's been instantiated using PauseImage.
+	PauseCommand string `toml:"pause_command"`
 }
 
 // tomlConfig is another way of looking at a Config, which is

runcom/ocid-1

server/config.go
@@ -23,17 +23,20 @@ type Config struct {
 
 // RootConfig represents the root of the "ocid" TOML config table.
 type RootConfig struct {
-	// Root is a path to the "root directory" where all information not
+	// Root is a path to the "root directory" where data not
 	// explicitly handled by other options will be stored.
 	Root string `toml:"root"`
 
-	// SandboxDir is the directory where ocid will store all of its sandbox
-	// state and other information.
-	SandboxDir string `toml:"sandbox_dir"`
+	// RunRoot is a path to the "run directory" where state information not
+	// explicitly handled by other options will be stored.
+	RunRoot string `toml:"runroot"`
 
-	// ContainerDir is the directory where ocid will store all of its container
-	// state and other information.
-	ContainerDir string `toml:"container_dir"`
+	// Storage is the name of the storage driver which handles actually
+	// storing the contents of containers.
+	Storage string `toml:"storage_driver"`
+
+	// StorageOption is a list of storage driver specific options.
+	StorageOptions []string `toml:"storage_option"`
 
 	// LogDir is the default log directory were all logs will go unless kubelet
 	// tells us to put them somewhere else.
@@ -81,17 +84,15 @@ type RuntimeConfig struct {
 
 // ImageConfig represents the "ocid.image" TOML config table.
 type ImageConfig struct {
-	// Pause is the path to the statically linked pause container binary, used
-	// as the entrypoint for infra containers.
-	//
-	// TODO(cyphar): This should be replaced with a path to an OCI image
-	// bundle, once the OCI image/storage code has been implemented.
-	Pause string `toml:"pause"`
-
-	// ImageStore is the directory where the ocid image store will be stored.
-	// TODO: This is currently not really used because we don't have
-	//       containers/storage integrated.
-	ImageDir string `toml:"image_dir"`
+	// DefaultTransport is a value we prefix to image names that fail to
+	// validate source references.
+	DefaultTransport string `toml:"default_transport"`
+	// PauseImage is the name of an image which we use to instantiate infra
+	// containers.
+	PauseImage string `toml:"pause_image"`
+	// PauseCommand is the path of the binary we run in an infra
+	// container that's been instantiated using PauseImage.
+	PauseCommand string `toml:"pause_command"`
 }
 
 // NetworkConfig represents the "ocid.network" TOML config table